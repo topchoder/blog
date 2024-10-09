2c08090da61fdf80ad8180a69e9c81285b54621c
Anurag Maurya <anuragmauryag@gmail.com>
2024-10-09 06:50:19 -0700

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from django.contrib.auth import views as auth_views
+from .views import blog_list, blog_detail
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('signup/', SignupView.as_view(), name='signup'),  # Create this view
+    path('', blog_list, name='blog_list'),
+    path('blog/<int:blog_id>/', blog_detail, name='blog_detail'),
 ]
